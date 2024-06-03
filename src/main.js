1054b86e96dd0c3b2ebafd2f8dd938dfa6b3bdc3
Niggha2003 <nguyennghia241003@gmail.com>
2024-06-03 16:34:14 +0700

    Cài đặt thư viện và khởi tạo

--- src/main.js
+++ src/main.js
@@ -1,11 +1,27 @@
-import './assets/main.css'
+import './assets/css/main.css'
+import './assets/scss/sidebar.scss'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js' // Nếu bạn cần sử dụng các tính năng JavaScript của Bootstrap
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+
+/* import prime vue lib */
+import PrimeVue from 'primevue/config'
+//css for prime vue
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primevue/resources/primevue.min.css' /* Deprecated */
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const app = createApp(App)
-
+app.use(store)
 app.use(router)
+app.use(PrimeVue, { ripple: true })
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
